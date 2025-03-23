2d2715de41c9a38a1c76574b903121aa9aac9466
Anouer <107050724+AnouerMokrane@users.noreply.github.com>
2025-03-23 21:26:05 +0100

    Update home page to find the error on deployment

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import BlogSection from "@/components/BlogSection";
+//import BlogSection from "@/components/BlogSection";
 import Hero from "@/components/Hero";
 import { Suspense } from "react";
 
@@ -12,16 +12,7 @@ export default async function Home({
 
   return (
     <>
-      <Hero />
-      <Suspense
-        fallback={
-          <div className="flex  justify-center h-[calc(100dvh-(150px+81px))] sm:h-[calc(100dvh-(150px+150px))]pt-28">
-            <span className="w-12 h-12 border-4 border-dashed rounded-full animate-spin"></span>
-          </div>
-        }
-      >
-        <BlogSection category={currentCategory} />
-      </Suspense>
+     <Hero />
     </>
   );
 }
