021c4a409c1c8409d64b39c057c6792bf2f1a0cf
anouermokrane <anouermokrane@gmail.com>
2025-03-23 16:55:57 +0100

    fix: parse blog content correctly in blog details page

--- src/app/(root)/blogs/[id]/page.tsx
+++ src/app/(root)/blogs/[id]/page.tsx
@@ -23,7 +23,7 @@ export default async function page({
 
   const blogDetails: BlogPost = data.data;
 
-  const htmlContent = marked.parse("");
+  const htmlContent = marked.parse(blogDetails.content || "");
 
   return (
     <>
